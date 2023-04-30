42a6abbd026b4991c4ac64e6d789541c478ceac3
Yaroslav Mykhailychenko <arostar197541@gmail.com>
2023-04-30 19:08:24 +0300

    Initialize class to add big numbers

---
+++ addLongNumber.cpp
@@ -0,0 +1,11 @@
+#include "addLongNumber.h"
+#include <iostream>
+
+using namespace std;
+
+void addLongNumber::printNumber() {
+    cout << "Default constructor class \n";
+    cout << "Input pls first element in numeric \n";
+
+    while
+}
---
+++ addLongNumber.h
@@ -0,0 +1,25 @@
+#ifndef SUMMNUMBERS_ADDLONGNUMBER_H
+#define SUMMNUMBERS_ADDLONGNUMBER_H
+
+
+
+struct longNumberElement {
+    longNumberElement *next, *prev;
+    int data;
+};
+
+using namespace std;
+using longNumber = longNumberElement*;
+
+class addLongNumber {
+private:
+    longNumber firstNumber, secondNumber;
+public:
+    addLongNumber();
+    addLongNumber(char numberPerLine);
+
+    void printNumber();
+};
+
+
+#endif
